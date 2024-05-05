e3b9dd1169a871e2933d422242439ee0de1da562
Betul <betulkoru06@gmail.com>
2024-05-05 18:05:38 +0300

    footer

--- src/components/Footers.jsx
+++ src/components/Footers.jsx
@@ -1,9 +1,84 @@
-import React from 'react'
+import * as React from "react";
+import Container from "@mui/material/Container";
 
-const Footers = () => {
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+
+import { IconButton } from "@mui/material";
+
+function Footer() {
   return (
-    <div>Footers</div>
-  )
+    <Paper
+      sx={{
+        // marginTop: "calc(10% + 60px)",
+        width: "100%",
+        // position: "fixed",
+        bottom: 0,
+
+        backgroundColor: "primary.main",
+
+        color: "white",
+      }}
+      component="footer"
+      square
+      variant="outlined"
+    >
+      <Container maxWidth="lg">
+        <Box
+          sx={{
+            flexGrow: 1,
+            justifyContent: "center",
+            display: "flex",
+            my: 1,
+          }}
+        >
+          <div>
+            {/* <Image priority src="/Logo.svg" width={75} height={30} alt="Logo" /> */}
+          </div>
+        </Box>
+
+        <Box
+          sx={{
+            flexGrow: 1,
+            justifyContent: "center",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            mb: 2,
+          }}
+        >
+          <Typography>Developed By Betul Koru</Typography>
+          <Box>
+            <IconButton
+              sx={{ color: "white" }}
+              onClick={() => window.open("https://github.com/psyBeth")}
+            >
+              <GitHubIcon />
+            </IconButton>
+            <IconButton
+              sx={{ color: "white" }}
+              onClick={() =>
+                window.open("https://www.linkedin.com/in/betul-koru-2303192ab/")
+              }
+            >
+              <LinkedInIcon />
+            </IconButton>
+            {/* <IconButton sx={{ color: "white" }} onClick={() => window.open("")}>
+              <FacebookIcon />
+            </IconButton> */}
+          </Box>
+
+          <Typography variant="caption">
+            Copyright ©️ {new Date().getFullYear()}
+          </Typography>
+        </Box>
+      </Container>
+    </Paper>
+  );
 }
 
-export default Footers
+export default Footer;
