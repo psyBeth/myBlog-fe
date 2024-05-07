0e37a4fe1bf45b76eb2165d734053d2d78598bfb
Betul <betulkoru06@gmail.com>
2024-05-07 22:32:38 +0300

    components + runtime errors

--- src/components/blog/CommentForm.jsx
+++ src/components/blog/CommentForm.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import { TextField, Button, Box } from "@mui/material";
 
-const CommentForm = () => {
+import { useState } from "react";
+import useBlogCalls from "../../hooks/useBlogCalls";
+
+const CommentForm = ({ postId }) => {
+  const { addComment } = useBlogCalls();
+
+  const [comment, setComment] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = { post: postId, content: comment };
+    addComment(`comments/${postId}`, data);
+    e.target.reset();
+  };
+
   return (
-    <div>CommentForm</div>
-  )
-}
+    <Box component="form" onSubmit={handleSubmit}>
+      <TextField
+        label="Comment"
+        name="comment"
+        id="comment"
+        type="text"
+        variant="outlined"
+        multiline
+        size="medium"
+        rows={4}
+        fullWidth
+        onChange={(e) => setComment(e.target.value)}
+      />
+      <Button type="submit" variant="contained" sx={{ mt: 2 }}>
+        Add New Comment
+      </Button>
+    </Box>
+  );
+};
 
-export default CommentForm
+export default CommentForm;
