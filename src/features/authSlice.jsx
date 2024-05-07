bc5a982f579defc6de9db44e71a051d610f2c218
Betul <betulkoru06@gmail.com>
2024-05-07 22:21:36 +0300

    components form the old blog app

--- src/features/authSlice.jsx
+++ src/features/authSlice.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import { createSlice } from "@reduxjs/toolkit";
 
-const authSlice = () => {
-  return (
-    <div>authSlice</div>
-  )
-}
+const authSlice = createSlice({
+  name: "auth",
 
-export default authSlice
+  initialState: {
+    currentUser: null,
+    loading: false,
+    error: false,
+    token: null,
+  },
+  reducers: {
+    fetchStart: (state) => {
+      state.loading = true;
+      state.error = false;
+    },
+    loginSuccess: (state, { payload }) => {
+      state.loading = false;
+      state.currentUser = payload?.user;
+      state.token = payload?.key;
+    },
+    logoutSuccess: (state) => {
+      state.loading = false;
+      state.currentUser = null;
+      state.token = null;
+    },
+    registerSuccess: (state, { payload }) => {
+      state.loading = false;
+      state.currentUser = payload?.user;
+      state.error = false;
+    },
+    fetchFail: (state) => {
+      state.loading = false;
+      state.error = true;
+    },
+  },
+});
+export const {
+  fetchStart,
+  fetchFail,
+  loginSuccess,
+  logoutSuccess,
+  registerSuccess,
+} = authSlice.actions;
+export default authSlice.reducer;
--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import axios from "axios";
 
 const useAxios = () => {
-  return (
-    <div>useAxios</div>
-  )
-}
-
-export default useAxios
+  const token = "96f323b5ad899474749810016cf99810e0a432fe";
+  const axiosPublic = axios.create({
+    baseURL: "https://32131.fullstack.clarusway.com/",
+  });
+  const axiosWithToken = axios.create({
+    baseURL: "https://32131.fullstack.clarusway.com/",
+    headers: { Authorization: `Token ${token}` },
+  });
+  return { axiosPublic, axiosWithToken };
+};
+export default useAxios;
